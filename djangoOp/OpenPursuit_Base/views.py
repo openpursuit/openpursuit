889f6f5baccd4feff25a81bc03b3bda004e97a7d
Saverio Proto <zioproto@gmail.com>
2007-12-15 23:12:01 +0000

    Migliorata logica inserimento domande database
    Se il tag non esiste viene aggiunto al DB
    
    
    
    git-svn-id: https://svn.ninux.org/svn/ninuxdeveloping/openpursuit/trunk@289 ef3c9aa7-e928-0410-9a28-8491e62b674b

--- djangoOp/OpenPursuit_Base/views.py
+++ djangoOp/OpenPursuit_Base/views.py
@@ -5,6 +5,7 @@ from django import newforms as forms
 from djangoOp.OpenPursuit_Base.models import Tags
 from djangoOp.OpenPursuit_Base.models import Question 
 from djangoOp.OpenPursuit_Base.models import Answers
+from django.core.exceptions import ObjectDoesNotExist
 
 def index(request):
 
@@ -22,25 +23,29 @@ def index(request):
         
 		form = QuestionForm(request.POST)
 	        if form.is_valid():
-			#FIXME
 			# Do form processing here...	                
 			
 			#WARNING --- FIX THIS --- THIS LOGIC DOES NOT WORK
 			
 			#Se il tag non esiste lo aggiungo
-			
-			#FIXME: "musica" va cambiato con il TAG passato via POST
-			t = Tags.objects.create(tag="musica")
-			t.save()
-			
-			q = Question(question = 'x', difficulty = ' ', score = ' ', date = ' ');
-			q.tag.add(t)
-			q.save()
-			
-			p = Answers(question = q, right1='sdfds' , wrong1='sdfsdf', wrong2='cdsfs', wrong3='xx')
-			p.save()
+			try:
+				t = Tags.objects.get(tag=request.POST['tags'])
 
-			return HttpResponseRedirect('/url/on_success/')
+			except ObjectDoesNotExist:
+				
+				t = Tags.objects.create(tag=request.POST['tags'])
+				t.save()
+
+			
+			#q = Question(question = 'x', difficulty = ' ', score = ' ', date = ' ');
+			#q.tag.add(t)
+			#q.save()
+			#
+			#p = Answers(question = q, right1='sdfds' , wrong1='sdfsdf', wrong2='cdsfs', wrong3='xx')
+			#p.save()
+
+			#return HttpResponseRedirect('/url/on_success/')
+			return HttpResponse("OK QUESTION ADDED")
 	else:
 		#template = loader.get_template('form.html')
 		form = QuestionForm()
