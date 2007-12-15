f75eed0c174850edd3330796f2dbfb5c560a9ba9
Saverio Proto <zioproto@gmail.com>
2007-12-15 22:45:41 +0000

    Ho lavorato su views.py dove avevo fatto una cosa schifosa
    Adesso ci sta una sola funziona, che se vede un metodo POST processa i dati.
    
    Se invece il metodo non e' POST fa apparire il FORM per l'inserimento della domanda.
    
    Un template minimale form.html e' stato aggiunto.
    
    
    
    git-svn-id: https://svn.ninux.org/svn/ninuxdeveloping/openpursuit/trunk@288 ef3c9aa7-e928-0410-9a28-8491e62b674b

--- djangoOp/OpenPursuit_Base/views.py
+++ djangoOp/OpenPursuit_Base/views.py
@@ -1,59 +1,52 @@
 # Create your views here.
 from django.http import HttpResponse
-
-
-#FIXME
-#questa index va totalmente cambiata
-
-def index(request):
-
-	output = "  \
-<html> \
-<head>\
- <title>Aggiungi nuova domanda</title>\
-</head>\
-<body>\
-<b><font color=\"red\" size=\"5\"><?=$msg;?></font></b>\
-<form action=\"addQuestionDo.php\" method=\"post\">\
-<div align=\"center\"><table align=\"left\" border=\"0\" cellspacing=\"0\" cellpadding=\"3\"> \
-Question:<input type=\"text\" size=\"30\" maxlength=\"36\" name=\"question\">:<br />Right Answer:<input type=\"text\" size=\"30\" maxlength=\"36\" name=\"right1\">:<br />\
-	Wrong Answer1:<input type=\"text\" size=\"30\" maxlength=\"36\" name=\"wrong1\">:<br />\
-	Wrong Answer2:<input type=\"text\" size=\"30\" maxlength=\"36\" name=\"wrong2\">:<br />\
-	Wrong Answer3:<input type=\"text\" size=\"30\" maxlength=\"36\" name=\"wrong3\">:<br />\
-	Difficulty:<input type=\"text\" size=\"12\" maxlength=\"36\" name=\"difficulty\">:<br />\
-	Tags (separati da virgole)<input type=\"text\" size=\"12\" maxlength=\"36\" name=\"tags\">:<br />\
-<tr><td colspan=\"2\" align=\"right\"><input type=\"submit\" name=\"action\" value=\"addquestion\"></td></tr></table>\
-</div>\
-</form>\
-</body>\
-</html>\
-	";
-return HttpResponse(output)
-
-
-#FIXME
-#questo Ã¨ solo uno scheletro per aggiungere una domanda passata tramite POST
-
-def addQuestion(request):
-
+from django.shortcuts import render_to_response
+from django import newforms as forms
 from djangoOp.OpenPursuit_Base.models import Tags
 from djangoOp.OpenPursuit_Base.models import Question 
 from djangoOp.OpenPursuit_Base.models import Answers
 
-#TODO
-#Check se il tag specificato non esiste gia
+def index(request):
 
-#Se il tag non esiste lo aggiungo
+	class QuestionForm(forms.Form):
+		question = forms.CharField(max_length=2000)
+		rightAnswer = forms.CharField(max_length=2000)
+		wrongAnswer1 = forms.CharField(max_length=2000)
+		wrongAnswer2 = forms.CharField(max_length=2000)
+		wrongAnswer3 = forms.CharField(max_length=2000)
+		difficulty = forms.IntegerField()
+		tags = forms.CharField(max_length=2000)
 
-#FIXME: "musica" va cambiato con il TAG passato via POST
-t = Tags.objects.create(tag="musica")
-t.save()
 
-q = Question(question = 'x', difficulty = ' ', score = ' ', date = ' ');
-q.tag.add(t)
-q.save()
+	if request.method == 'POST':
+        
+		form = QuestionForm(request.POST)
+	        if form.is_valid():
+			#FIXME
+			# Do form processing here...	                
+			
+			#WARNING --- FIX THIS --- THIS LOGIC DOES NOT WORK
+			
+			#Se il tag non esiste lo aggiungo
+			
+			#FIXME: "musica" va cambiato con il TAG passato via POST
+			t = Tags.objects.create(tag="musica")
+			t.save()
+			
+			q = Question(question = 'x', difficulty = ' ', score = ' ', date = ' ');
+			q.tag.add(t)
+			q.save()
+			
+			p = Answers(question = q, right1='sdfds' , wrong1='sdfsdf', wrong2='cdsfs', wrong3='xx')
+			p.save()
 
-p = Answers(question = q, right1='sdfds' , wrong1='sdfsdf', wrong2='cdsfs', wrong3='xx')
-p.save()
+			return HttpResponseRedirect('/url/on_success/')
+	else:
+		#template = loader.get_template('form.html')
+		form = QuestionForm()
+		return render_to_response('form.html', {'form': form})
+
+
+
 
 
