0d06d9462eb3f83c598e027f16e8902a8e798fed
Lorenzo Bracciale <ziducaixao@autistici.org>
2010-06-01 10:19:43 +0000

    patched to admin newforms
    git-svn-id: https://svn.ninux.org/svn/ninuxdeveloping/openpursuit/trunk@781 ef3c9aa7-e928-0410-9a28-8491e62b674b

--- djangoOp/urls.py
+++ djangoOp/urls.py
@@ -1,14 +1,16 @@
 from django.conf.urls.defaults import *
+from django.contrib import admin
 from django.conf import settings
 import amf.django
 
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
     # (r'^djangoOp/', include('djangoOp.foo.urls')),
 
-    # Uncomment this for admin:
-     (r'^admin/', include('django.contrib.admin.urls')),
+     (r'^admin/(.*)', admin.site.root),
+     #(r'^admin/', admin.site.root, ),
      (r'^$', 'djangoOp.op.views.index'),
 #     (r'^intro$', 'djangoOp.op.views.intro'),
      (r'^static/(.*)$', 'django.views.static.serve',
