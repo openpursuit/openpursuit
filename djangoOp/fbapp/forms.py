9b580bdb09faaa43cda46fdd8cb889a7cca4ab93
Lorenzo Bracciale <ziducaixao@autistici.org>
2010-12-25 20:05:43 +0000

    chart
    git-svn-id: https://svn.ninux.org/svn/ninuxdeveloping/openpursuit/trunk@878 ef3c9aa7-e928-0410-9a28-8491e62b674b

---
+++ djangoOp/fbapp/forms.py
@@ -0,0 +1,33 @@
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django import forms
+from djangoOp.op.models import *
+from djangoOp.settings import MEDIA_ROOT
+from django.core.exceptions import ObjectDoesNotExist
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import redirect_to_login
+import datetime
+from  django.utils import simplejson
+from djangoOp.widgets.autocomplete import AutoCompleteWidget
+from django.conf.urls.defaults import *
+from django.core.urlresolvers import reverse
+
+
+class FBQuizForm(forms.Form):
+	question = forms.CharField(max_length=2000)
+	rightAnswer = forms.CharField(max_length=2000)
+	wrongAnswer1 = forms.CharField(max_length=2000)
+	wrongAnswer2 = forms.CharField(max_length=2000)
+	wrongAnswer3 = forms.CharField(max_length=2000)
+	difficulty = forms.ChoiceField(choices=DIFFICULTY_LEVEL)
+	tags = forms.CharField(max_length=2000)
+	reference = forms.URLField(max_length=2000)
+	
+	def __init__(self,*args, **kwargs ):
+		super(FBQuizForm, self).__init__(*args, **kwargs)
+		n_lookup_url = reverse('djangoOp.op.views.tags_ac')  # url to your view
+		n_schema = '["resultset.results", "tag", "occurrencies" ]'
+		self.fields['tags'].widget = AutoCompleteWidget()
+		self.fields['tags'].widget.lookup_url = n_lookup_url
+		self.fields['tags'].widget.schema = n_schema
--- djangoOp/fbapp/views.py
+++ djangoOp/fbapp/views.py
@@ -4,12 +4,20 @@ from django.views.generic.simple import direct_to_template
 #if you dont want to use a decorator instead of the middleware
 #from django.utils.decorators import decorator_from_middleware
 #from facebook.djangofb import FacebookMiddleware
-
+import urllib
+from django.core.files import File
+from djangoOp.op.models import *
+import os
+from django.conf import settings
+from django.forms import ModelForm
+from django.contrib.auth.models import User
 # Import the Django helpers
-import facebook.djangofb as facebook
+# import facebook.djangofb as facebook
 
 # The User model defined in models.py
-from models import User
+#from models import User
+from forms import FBQuizForm
+from django.db.models import Avg, Min, Max, Count
 
 # We'll require login for our canvas page. This
 # isn't necessarily a good idea, as we might want
@@ -17,33 +25,136 @@ from models import User
 # access to their info. See the wiki for details on how
 # to do this.
 
-def channel(request):
-    return direct_to_template(request, 'fbapp/channel.html', extra_context={})
 
 
 def main(request):
-    return direct_to_template(request, 'fbapp/main.html', extra_context={})
+    fbuser = None
+    if request.method == 'GET':
+        uid = request.GET.get('uid', None)
+        bestof = 0
+        if uid and uid != '0':
+              fbuser = FBProfile.objects.get(uid=uid)
+              best_tags = []
+              for t in Tags.objects.all():
+                  for ts in TagsScore.objects.filter(tag=t).order_by('-score')[:1]:
+                         if ts.user == fbuser:
+                             best_tags.append(t.tag)
+                             bestof = bestof +1
+              #try:
+              score = 0
+              tss = TagsScore.objects.filter(user=fbuser)
+              for i in tss:
+                  score = score + i.score
+              #aggr = TagsScore.objects.filter(user=fbuser).annotate(average=Avg('score'), max=Max('score'), min=Min('score'), count=Count('score'))
+              #score = aggr[count] 
+              #raise score
+              #except:
+              fbuser.score = score
+              fbuser.bestof = bestof
+              if  bestof >  3:
+                  fbuser.best_tags = best_tags[:3] + [ " e altri" ]
+              else:
+                  fbuser.best_tags = best_tags 
+    return direct_to_template(request, 'fbapp/main.html', extra_context={'fbuser': fbuser})
+
+def save_score(request):
+    if request.method == 'POST':
+        uid = request.POST.get('uid', None)
+        scores = request.POST.getlist('scores[]')
+        fbuser = FBProfile.objects.get(uid=uid)
+        for e in scores:
+            t = Tags.objects.get(tag=e.split(':')[0])
+            try:
+               x = TagsScore.objects.get(user=fbuser, tag=t)
+               if x.score < e.split(':')[1] :
+                 x.score = e.split(':')[1]
+                 x.save()
+            except:
+               t = TagsScore(user=fbuser, tag=t, score=e.split(':')[1])
+               t.save()
+    return HttpResponse("Ok")
 
 def play(request):
     return direct_to_template(request, 'fbapp/play.html', extra_context={})
 
+
+
 def play2(request):
     return direct_to_template(request, 'fbapp/play2.html', extra_context={})
 
+def play3(request):
+    general_chart = [] 
+    friend_chart = []
+    tag_friend_chart = []
+    tag_general_chart = { } 
+    tags = request.GET.getlist('tag')
+    if request.method == 'GET':
+        uid = request.GET.get('uid', None)
+        if uid and uid != '0':
+            general_chart = FBProfile.objects.order_by( '-score')[:6]
+
+            tag_chart = []  
+            for t in tags:
+                qs = TagsScore.objects.filter(tag__tag=t).order_by( '-score')[:6]
+                for q in qs:
+                    el = { }
+                    el = {'uid': q.user.uid, 'tag' : q.tag, 'score' : q.score, 'first_name': q.user.first_name }
+                    tag_chart.append(el)
+                tag_general_chart[t] = tag_chart
+                tag_chart = [ ]
+    return direct_to_template(request, 'fbapp/play3.html', extra_context={'general_chart' : general_chart,'tag_general_chart': tag_general_chart })
+
+
 def addquiz(request):
-    return direct_to_template(request, 'fbapp/addquiz.html', extra_context={})
+    if request.method == 'POST':
+        post_data = request.POST.copy()
+        form = FBQuizForm(post_data)    
+    else:
+        form = FBQuizForm()
+    return direct_to_template(request, 'fbapp/addquiz.html', extra_context={'form':form})
 
 def challenge(request):
     return direct_to_template(request, 'fbapp/challenge.html', extra_context={})
+
+def fb_login(request):
+    if request.method == 'POST':
+        uid = request.POST.get('uid', None)
+        first_name = request.POST.get('first_name', None)
+        last_name = request.POST.get('last_name', None)
+        pic_url = request.POST.get('pic_url', None)
+        email = request.POST.get('email', None)
+        content = urllib.urlretrieve(pic_url)
+        fbuser = FBProfile()
+        fbuser.uid=uid
+        fbuser.first_name=first_name
+        fbuser.last_name=last_name
+        fbuser.pic_url=pic_url
+        fbuser.email=email
+        fbuser.score = 0
+        fbuser.pic=File(name=uid+ "." +pic_url[-3:], file=open(content[0]))
+        user = User(username='FB'+uid, first_name=first_name, last_name=last_name,email=email, password="0", is_staff=False, is_active=True, is_superuser=False)
+        try:
+            u = FBProfile.objects.get(uid=uid)
+            w = User.objects.get(username='FB'+uid)
+        #    if os.path.exists(settings.MEDIA_ROOT + 'fbpics/' +uid+ "." +pic_url[-3:]):
+        #        u.pic.delete()
+        except:
+            user.save()
+            fbuser.user=user
+            fbuser.save()
+        #fbuser.pic.save(uid+ "." +pic_url[-3:], File(open(content[0])) , save=True)
+        return HttpResponse("Ok")
+    else:
+        return HttpResponse("Non Ok")
+
 
 #@decorator_from_middleware(FacebookMiddleware)
 #@facebook.require_login()
 def canvas(request):
     # Get the User object for the currently logged in user
+    #user = None
     #user = User.objects.get_current()
-    #aa = dir(user)
-    #bb = user.id
-    #cc = user.language
+    
     # Check if we were POSTed the user's new language of choice
     #if 'language' in request.POST:
     #    user.language = request.POST['language'][:64]
@@ -52,7 +163,7 @@ def canvas(request):
     # User is guaranteed to be logged in, so pass canvas.fbml
     # an extra 'fbuser' parameter that is the User object for
     # the currently logged in user.
-    return direct_to_template(request, 'fbapp/canvas.fbml') #, extra_context={'fbuser': user})
+    return direct_to_template(request, 'fbapp/canvas.fbml')#  extra_context={'fbuser': user})
 
 #@facebook.require_login()
 def ajax(request):
